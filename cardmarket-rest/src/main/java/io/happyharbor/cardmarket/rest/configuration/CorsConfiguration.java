171d2b2dc8c1c53e322d94199566eb51e4ee0b9b
Giorgos Trichopoulos <george.trichopoulos@gmail.com>
2021-05-22 18:36:00 +0300

    Introduce SecurityProperties

--- cardmarket-rest/src/main/java/io/happyharbor/cardmarket/rest/configuration/CorsConfiguration.java
+++ cardmarket-rest/src/main/java/io/happyharbor/cardmarket/rest/configuration/CorsConfiguration.java
@@ -1,20 +1,26 @@
 package io.happyharbor.cardmarket.rest.configuration;
 
+import io.happyharbor.cardmarket.rest.property.SecurityProperties;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class CorsConfiguration {
+
+    private final SecurityProperties securityProperties;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins(securityProperties.getAllowedOrigins())
                         .allowedMethods("GET", "HEAD", "POST", "PUT");
             }
         };
